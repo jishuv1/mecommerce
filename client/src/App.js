fc4a5dac2bd2ce3eea2bded9f6de1931e650e669
jishuv1 <jigyashuverma@gmail.com>
2023-06-21 16:16:57 +0545

    added product detail page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
-import ProductList from './features/product-list/ProductList.js';
 import LoginPage from './pages/LoginPage.js';
 import SignupPage from './pages/SignupPage.js';
 import { createRoot } from 'react-dom/client';
@@ -13,8 +11,10 @@ import {
   Link,
 } from 'react-router-dom';
 import Home from './pages/Home.js';
-import Cart from './features/cart/Cart.js';
 import CartPage from './pages/CartPage.js';
+import Checkout from './pages/Checkout.js';
+import ProductDetail from './features/product-list/components/ProductDetail.js';
+import ProductDetailPage from './pages/ProductDetailPage.js';
 
 const router = createBrowserRouter([
   {
@@ -32,6 +32,14 @@ const router = createBrowserRouter([
   {
     path: '/cart',
     element: <CartPage />,
+  },
+  {
+    path: '/checkout',
+    element: <Checkout />,
+  },
+  {
+    path: '/product-detail',
+    element: <ProductDetailPage />,
   },
 ]);
 
